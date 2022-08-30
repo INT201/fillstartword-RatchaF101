b64339e221cf88aeadcab435132444b19f6891f0
Ratchapon <ratchapon.sang@kmutt.ac.th>
2022-08-30 22:55:33 +0700

    sent Hw01

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,19 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  if(word === undefined){
+    return console.log('undefined')
+  }
+  else if(word === null){
+    return console.log('undefined')
+  }
+  else if(word.includes(startWord) == true){
+    return console.log(`"${word}"`)
+  }
+  else{
+    return console.log(`"${startWord}${word}"`)
+  }
 }
 module.exports = fillStartWord
+
+ 
